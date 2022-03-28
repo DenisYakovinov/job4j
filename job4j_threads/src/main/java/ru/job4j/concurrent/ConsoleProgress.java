9345e72436ccae3295a238226e1aa303d58ce004
DenisYakovinov <d.yakovinov@mail.ru>
2022-03-28 14:32:13 +0300

    fix checkstyle thread interruption t3

--- job4j_threads/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ job4j_threads/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -6,7 +6,7 @@ public class ConsoleProgress implements Runnable {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
         try {
-            Thread.sleep(1000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -15,12 +15,20 @@ public class ConsoleProgress implements Runnable {
 
     @Override
     public void run() {
-        char[] chars = new char[] {'-','\\', '|', '/'};
+        char[] chars = new char[] {'-', '\\', '|', '/'};
         int counter = 0;
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
+                /*
+                Метод Thread.interrupt() (который сейчас в main) не выставляет флаг прерывания, если нить находится в режиме WAIT, JOIN.
+                В этом случае методы sleep(), join(), wait() выкинут исключение. Поэтому нужно дополнительно проставить флаг прерывания.
+                (здесь)
+                Эта схема является шаблоном. Если используются методы sleep(), join() или wait(), то нужно в блоке catch
+                вызвать прерывание.
+                 */
+                Thread.currentThread().interrupt();
             }
             if (counter == 4) {
                 counter = 0;
