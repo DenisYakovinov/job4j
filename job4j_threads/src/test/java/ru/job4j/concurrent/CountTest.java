86f37344c1178ae5bc50766128c4a04c8c0b0916
Denis Yakovinov <d.yakovinov@mail.ru>
2022-03-30 20:04:17 +0300

    JCIP. Настройка библиотеки

---
+++ job4j_threads/src/test/java/ru/job4j/concurrent/CountTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountTest {
+
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertEquals(2, count.get());
+    }
+}
